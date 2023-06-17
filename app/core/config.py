bc4a0c7e3f9f6df538f4f0f77e765b99ca96f5a3
Akhil Narang <me@akhilnarang.dev>
2023-06-17 22:02:05 +0530

    feat(config): add in DB configuration
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- app/core/config.py
+++ app/core/config.py
@@ -1,7 +1,31 @@
-from pydantic import BaseSettings
+from typing import Any
+
+from pydantic import BaseSettings, validator, PostgresDsn
 
 
 class Settings(BaseSettings):
+    # Our PyMySQL DB URI
+    DB_HOST: str
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_NAME: str
+    DB_PORT: int = 5432
+    DB_SSLMODE: str = "disable"
+    DB_URI: str = ""
+
+    @validator("DB_URI", pre=True)
+    def assemble_db_connection(cls, v: str, values: dict[str, Any]) -> str:
+        if isinstance(v, str) and v:
+            return v
+        return PostgresDsn.build(
+            scheme="postgresql",
+            user=values.get("DB_USER"),
+            password=values.get("DB_PASSWORD"),
+            host=values.get("DB_HOST"),
+            path=f"/{values.get('DB_NAME') or ''}",
+            port=str(values.get("DB_PORT") or 5432),
+        )
+
     class Config:
         env_file = ".env"
 
