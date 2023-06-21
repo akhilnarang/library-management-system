1fbb82bb2e8fa76e6f81519fb1d78a1b0834998e
Akhil Narang <me@akhilnarang.dev>
2023-06-21 19:42:36 +0530

    feat: setup rich logger
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- app/main.py
+++ app/main.py
@@ -1,10 +1,20 @@
+import logging
+
 from fastapi import FastAPI
+from rich.logging import RichHandler
 from starlette.requests import Request
 from starlette.responses import Response
 
 from app import exception_handlers, exceptions
 from app.api.v1.routes import router as api_v1_router
 from app.views.routes import router as views_router
+
+logging.basicConfig(
+    format="[%(levelname)s] (%(asctime)s) %(module)s:%(pathname)s:%(funcName)s:%(lineno)s:: %(message)s",
+    level=logging.INFO,
+    datefmt="%d-%m-%y %H:%M:%S",
+    handlers=[RichHandler(rich_tracebacks=True)],
+)
 
 app = FastAPI()
 app.include_router(router=api_v1_router, prefix="/api/v1")
