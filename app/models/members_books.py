977a032eafdeb3794df89dfacd19e60b71c8589a
Akhil Narang <me@akhilnarang.dev>
2023-06-22 21:35:26 +0530

    refactor(members_books): fixup table name
    `members_bookss` -> `members_books`
    
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- app/models/members_books.py
+++ app/models/members_books.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 from sqlalchemy import Column, DateTime, ForeignKey, Integer
-from sqlalchemy.orm import Mapped, relationship
+from sqlalchemy.orm import Mapped, declared_attr, relationship
 
 from app.db.base_class import Base
 from app.models.book import Book
@@ -10,6 +10,10 @@ from app.utils import current_time
 
 
 class MembersBooks(Base):
+    @declared_attr
+    def __tablename__(cls) -> str:
+        return "members_books"
+
     member_id: int = Column(Integer, ForeignKey("members.id"), primary_key=True)
     book_id: int = Column(Integer, ForeignKey("books.id"), primary_key=True)
     borrowed_on: datetime = Column(DateTime, default=current_time)
